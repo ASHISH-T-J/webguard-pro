1caee13997a02d6bd9ef76eda882a6a858d3b023
Shalu Suresh <133752260+Shalu-Suresh@users.noreply.github.com>
2025-04-06 20:47:27 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+import random
+
+def get_random_user_agent():
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
+    ]
+    return random.choice(user_agents)
